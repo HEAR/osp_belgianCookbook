faf6a42984e78b06078b8c5ccd580516e762cf75
RomainGoetz <goetz.romain@gmail.com>
2015-03-12 13:37:48 +0100

    Push1
    Banzai !

---
+++ assets/js/page.js
@@ -0,0 +1,38 @@
+$(function() {
+	
+	
+    // Choose the number of pages of the document
+    var nb_page = 180;
+
+    for (i = 1; i < nb_page; i++){
+        $("#master-page").clone().attr("id","page-"+i).insertBefore($("#master-page"));
+    }
+    $("#master-page").hide();
+
+    // Loads main content into <article id="my-story">
+    $("#my-story").load("https://lite6.framapad.org/p/yOOZ2wkDAb/export/txt");
+
+    // __________________________________ MOVEABLE ELEMENTS __________________________________ //
+    $("div.moveable").
+        append("<div class='properties button'>Properties</div>").
+        draggable({
+                cursor: "move",
+                stack: "div.moveable",
+        }).
+        resizable();
+
+    $('.properties').on('click', function() {
+        var top = $(this).parent().css('top');
+        var left = $(this).parent().css('left');
+        var width = $(this).parent().width();
+        var height = $(this).parent().height();
+        var p = new Popelt({
+            title: "Properties to copy/paste into this object's style attribute.",
+            content: 'style="top: ' + top + '; left: ' + left + '; width: ' + width + 'px; height: ' + height + 'px;"',
+        }).show();
+    });
+
+	$("#page-2 .body").clone().insertAfter($("#page-2 .body")[0]);
+	$("#page-2 .body").clone().insertAfter($("#page-2 .body")[0]);
+	
+});
